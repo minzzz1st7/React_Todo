21fd85a66f722aa13340d17965418bbc13c2746e
정민지 <minzzz1st7@naver.com>
2022-06-17 21:51:52 +0900

    initial commit

---
+++ src/app.jsx
@@ -0,0 +1,71 @@
+import React, { Component } from "react";
+import "./app.css";
+import Habits from "./components/habits";
+import Navbar from "./components/navbar";
+import HabitAddForm from "./components/habitAddForm";
+
+class App extends Component {
+  state = {
+    habits: [
+      { id: 1, name: "Reading", count: 0 },
+      { id: 2, name: "Running", count: 0 },
+      { id: 3, name: "Coding", count: 0 },
+    ],
+  };
+
+  handleIncrement = (habit) => {
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    habits[index].count++;
+    this.setState({ habits: habits });
+  };
+  handleDecrement = (habit) => {
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    const count = habits[index].count - 1;
+    habits[index].count = count > 0 ? count : 0;
+
+    this.setState({ habits: habits });
+  };
+  handleDelete = (habit) => {
+    const habits = this.state.habits.filter((item) => item.id !== habit.id);
+    this.setState({ habits });
+  };
+
+  handleAdd = (name) => {
+    // 기존의 habits 데이터를 가져온 다음, 새로운 습관들도 추가해준다.
+    const habits = [
+      ...this.state.habits,
+      { id: Date.now(), name: name, count: 0 },
+    ];
+    this.setState({ habits });
+  };
+
+  handleReset = () => {
+    const habits = this.state.habits.map((habit) => {
+      habit.count = 0;
+      return habit;
+    });
+    this.setState({ habits });
+  };
+
+  render() {
+    return (
+      <>
+        <Navbar
+          totalCount={this.state.habits.filter((item) => item.count > 0).length}
+        />
+        <HabitAddForm onAdd={this.handleAdd} />
+        <Habits
+          habits={this.state.habits}
+          onIncrement={this.handleIncrement}
+          onDecrement={this.handleDecrement}
+          onDelete={this.handleDelete}
+          onResetAll={this.handleReset}
+        />
+      </>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/habits.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import Habit from "./habit";
+
+class Habits extends Component {
+  handleIncrement = (habit) => {
+    this.props.onIncrement(habit);
+  };
+  handleDecrement = (habit) => {
+    this.props.onDecrement(habit);
+  };
+  handleDelete = (habit) => {
+    this.props.onDelete(habit);
+  };
+
+  handleReset = () => {
+    this.props.onResetAll();
+  };
+
+  render() {
+    return (
+      <>
+        <ul>
+          {this.props.habits.map((habit) => (
+            // 연두색 habit이라는 prop 이름에 주황색 habit의 데이터를 전달해주는 것
+            <Habit
+              key={habit.id}
+              habit={habit}
+              onIncrement={this.props.onIncrement}
+              onDecrement={this.handleDecrement}
+              onDelete={this.handleDelete}
+            />
+          ))}
+        </ul>
+        <button className="habits-reset" onClick={this.handleReset}>
+          Reset All
+        </button>
+      </>
+    );
+  }
+}
+
+export default Habits;
